05e5d6aafaa3b2b31b763a6ebb54a3eb3e484be5
Joshua Scarsbrook <jscarsbrook@gmail.com>
2025-05-21 12:51:09 +1000

    moved fonts into seperate file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { GeistMono, GeistSans } from "@/components/fonts";
 
 export const metadata: Metadata = {
   title: "Neurocontainers Builder",
@@ -25,7 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${GeistSans.variable} ${GeistMono.variable} antialiased`}
       >
         {children}
       </body>
